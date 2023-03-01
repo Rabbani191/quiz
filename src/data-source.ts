8ed918e1af7b693fbdc7d585a732bb2981eab08f
Rabbani191 <umar.rabbani.191@gmail.com>
2023-03-01 17:02:34 +0000

    Update Package

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,12 +5,8 @@ import { Question } from "./entity/question.entity";
 import { Choice } from "./entity/choice.entity";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.DB_HOST,
-    port: 5432,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    type: "sqlite",
+    database: "quiz.sqlite",
     synchronize: true,
     logging: true,
     entities: [Choice, Question, Quiz ],
